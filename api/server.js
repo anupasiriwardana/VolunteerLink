2f5b68e75540c085c134ca5b918977d9e93c6ce3
Anupa Siriwardana <anupasiriwardana123@gmail.com>
2024-08-20 14:46:39 +0530

    Updated the directory structure

---
+++ api/server.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+
+
+//listen for requests
+app.listen(process.env.PORT, () => {
+    console.log("listening on port 4000");
+});
